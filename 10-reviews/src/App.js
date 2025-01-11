5a919478bdee2a840069c1580c0bbc54b44f61f3
Selva_lr <84980271+selvalr@users.noreply.github.com>
2025-01-11 07:32:30 +0000

    reviews project using react

---
+++ 10-reviews/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Reviews from './Reviews';
+
+function App() {
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+          </div>
+          <Reviews/>
+          </section>
+          </main>
+  );
+}
+
+export default App;
