ca4092603115190924d7c4f924257d3db0e0c606
Selva_lr <84980271+selvalr@users.noreply.github.com>
2025-05-23 10:40:20 +0000

    disny

---
+++ 18-Disney-Plus-Clone/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/Header'
+import Login from './components/Login'
+
+function App() {
+ 
+  return (
+    <>
+
+      <div className="app">
+        <Header/>
+<Login/>
+      </div>
+    </>
+  )
+}
+
+export default App
