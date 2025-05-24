b868d6648382ed795956b1acc028259d68278459
Selva_lr <84980271+selvalr@users.noreply.github.com>
2025-05-24 11:20:34 +0000

    adding

--- 18-Disney-Plus-Clone/src/App.jsx
+++ 18-Disney-Plus-Clone/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home/>}/>
           </Routes>
         </Router>
       </div>
---
+++ 18-Disney-Plus-Clone/src/components/ImageSlider.jsx
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import firstSliderImg from "../assets/images/slider-badging.jpg";
+import secondSliderImg from "../assets/images/slider-scale.jpg";
+import thirdSliderImg from "../assets/images/slider-badag.jpg"
+import fourSliderImg from "../assets/images/slider-scales.jpg"
+
+const ImgageSlider = (props) => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <a>
+          <img src={firstSliderImg} alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src={secondSliderImg} alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src={thirdSliderImg} alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src={fourSliderImg} alt="" />
+        </a>
+      </Wrap>
+    </Carousel>
+  );
+};
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+  li.slick-active button:before {
+    color: white;
+  }
+  .slick-list {
+    overflow: initial;
+  }
+  .slick-prev {
+    left: -75px;
+  }
+  .slick-next {
+    right: -75px;
+  }
+`;
+
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+  a {
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    &:hover {
+      padding: 0;
+      border: 4px solid rgba(249, 249, 249, 0.8);
+      transition-duration: 300ms;
+    }
+  }
+`;
+
+export default ImgageSlider;
