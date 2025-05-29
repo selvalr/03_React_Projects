fb0ad7c306810f6a695b534e25a5ea7fcc06b48f
Selva_lr <84980271+selvalr@users.noreply.github.com>
2025-05-29 12:02:20 +0000

    disny

--- 18-Disney-Plus-Clone/src/components/Home.jsx
+++ 18-Disney-Plus-Clone/src/components/Home.jsx
@@ -1,19 +1,73 @@
 import React from 'react'
 import styled from 'styled-components'
-import ImageSlider from './ImageSlider';
-
-
-
-
+import ImgSlider from './ImageSlider';
+import NewDisney from './NewDisney';
+import Originals from './Originals';
+import Recommends from './Recommends';
+import Trending from './Trending';
+import Viewers from './Viewers';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from '../features/user/useSlice';
+import { getFirestore } from "firebase/firestore";
+import { collection, doc, setDoc, getDocs } from 'firebase/firestore'
 
 const Home = () => {
+const db = getFirestore()
+
+const moviesRef = collection(db, 'movies')
+
+
+  const dispatch = useDispatch()
+  const userName = useSelector(selectUserName)
+
+  let recommends = []
+  let newDisneys = []
+  let originals = []
+  let trending = []
+
+  useEffect(() => {
+    getDocs(moviesRef).then((snapshot) => {
+      snapshot.docs.map((doc) => {
+        switch(doc.data().type){
+          case 'recommend':
+            recommends = [...recommends, ({id: doc.id, ...doc.data()})]
+            break;
+          case 'new':
+            newDisneys = [...newDisneys, ({id: doc.id, ...doc.data()})]
+            break;
+          case 'original':
+            originals = [...originals, ({id: doc.id, ...doc.data()})]
+            break;
+          case 'trending':
+            trending = [...trending, ({id: doc.id, ...doc.data()})]
+            break;
+        }
+      })
+  //   })
+
+  dispatch(setMovies({
+    recommend:recommends,
+    newDisney:newDisneys,
+    original:originals,
+    trending:trending,
+    
+  }))
+}, [userName])}
+  )
+
   return (
     <Container>
-        <ImageSlider/>
+        <ImgSlider />
+        <Viewers />
+        <Recommends />
+        <NewDisney />
+        <Originals />
+        <Trending />
     </Container>
   )
 }
-
 
 const Container = styled.main`
   position: relative;
@@ -32,6 +86,5 @@ const Container = styled.main`
     z-index: -1;
   }
 `;
-
 
 export default Home
