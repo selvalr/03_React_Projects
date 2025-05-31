4c9fa3e5ea2e2478b292bf109f8d3798ffa91936
Selva_lr <84980271+selvalr@users.noreply.github.com>
2025-05-31 11:06:30 +0000

    a

--- 21-Login-React/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-
-export default axios.create({
- baseURL: 'http://localhost:3500'
-    //baseURL: 'https://obscure-eureka-9496qvxwg5vf99g5-3500.app.github.dev',
- // withCredentials: true 
-})
